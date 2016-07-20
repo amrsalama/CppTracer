b055bdd8a0eb51c2f3e49a965c2fdb60dbe29edc
Amr Salama <amr.mo.salama@gmail.com>
2016-07-20 19:09:14 +0200

    Add stringify utility function

--- include/tracer/utility/general.h
+++ include/tracer/utility/general.h
@@ -1,9 +1,22 @@
 #ifndef INCLUDE_TRACER_UTILITY_GENERAL_H_
 #define INCLUDE_TRACER_UTILITY_GENERAL_H_
 
+#include <string>
+#include <sstream>
+
 namespace tracer {
 namespace utility {
 namespace general {
+
+  template<typename T>
+  inline std::string stringify(const T &data) {
+      std::stringstream string_stream;
+      std::string converted_data;
+
+      string_stream << data;
+      string_stream >> converted_data;
+      return converted_data;
+  }
 
 }  // namespace general
 }  // namespace utility
