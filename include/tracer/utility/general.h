d4132deedc98b3df6fd47bea84c05ae7569efb10
Amr Salama <amr.mo.salama@gmail.com>
2016-07-20 04:34:20 +0200

    Add general/gl utilities

---
+++ include/tracer/utility/general.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_TRACER_UTILITY_GENERAL_H_
+#define INCLUDE_TRACER_UTILITY_GENERAL_H_
+
+namespace tracer {
+namespace utility {
+namespace general {
+
+}  // namespace general
+}  // namespace utility
+}  // namespace tracer
+
+#endif  // INCLUDE_TRACER_UTILITY_GENERAL_H_
---
+++ include/tracer/utility/gl.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_TRACER_UTILITY_GL_H_
+#define INCLUDE_TRACER_UTILITY_GL_H_
+
+namespace tracer {
+namespace utility {
+namespace gl {
+
+}  // namespace gl
+}  // namespace utility
+}  // namespace tracer
+
+#endif  // INCLUDE_TRACER_UTILITY_GL_H_
