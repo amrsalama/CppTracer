886e6147d9dbda220663b2dd83f993e697291514
Amr Salama <amr.mo.salama@gmail.com>
2016-07-20 03:38:24 +0200

    Add tracer constructor/destructor

--- include/tracer/tracer.h
+++ include/tracer/tracer.h
@@ -1,13 +1,28 @@
 #ifndef INCLUDE_TRACER_TRACER_H_
 #define INCLUDE_TRACER_TRACER_H_
 
+#include <string>
 #include "GL/freeglut.h"
 #include "GL/gl.h"
 
+// The base tracer class
 class Tracer {
  public:
-  Tracer(int argc, char** argv, float window_width, float windw_height);
+  Tracer(int argc,                  // [argc, argv] are required
+        char** argv,                // by glut libarary (glutInit).
+        float window_width,
+        float window_height,
+        std::string window_title);
   ~Tracer();
+ protected:
+  float window_width_;
+  float window_height_;
+ private:
+  const float kWindowMargin;        // Margin between tracers window.
+  static int window_count_;         // The number of created tracers.
+  static float window_x_position_;  // The x-position of the next tracer.
+  static float window_y_position_;  // The y-position of the next tracer.
+  int window_id_;
 };
 
 #endif  // INCLUDE_TRACER_TRACER_H_
--- src/tracer/tracer.cc
+++ src/tracer/tracer.cc
@@ -1,7 +1,48 @@
+#include <string>
 #include "tracer/tracer.h"
 
-Tracer::Tracer(int argc, char** argv, float window_width, float windw_height) {
+// Initialization of the static shared variables among all tracers.
+int Tracer::window_count_ = 0;
+float Tracer::window_x_position_ = 100;
+float Tracer::window_y_position_ = 100;
+
+Tracer::Tracer(int argc,
+              char** argv,
+              float window_width,
+              float window_height,
+              std::string window_title) : kWindowMargin(50.0) {
+  window_width_ = window_width;
+  window_height_ = window_height;
+
+  // Only call glutInit function first time the tracer is created,
+  // this allows multiple tracers to be created with glut been
+  // initalized only once.
+  if (window_count_++ == 0)
+    glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE);
+  glutInitWindowSize(window_width_, window_height_);
+  glutInitWindowPosition(window_x_position_, window_y_position_);
+  window_id_ = glutCreateWindow(window_title.c_str());
+
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, window_width_, 0, window_height_, 0, 1.0);
+
+  // Increse the y-position by the window height + a constant margin,
+  // this allows the multiple tracers to have consistent positions.
+  // TODO(@salama): Find a better placement method.
+  window_y_position_ += (window_height_ + kWindowMargin);
 }
 
 Tracer::~Tracer() {
+  window_count_--;
+  window_y_position_ -= (window_height_ + kWindowMargin);
+
+  // Hide the current tracer window and exit glut
+  // whenever there are no more tracers.
+  glutSetWindow(window_id_);
+  glutHideWindow();
+  if (window_count_ == 0)
+    glutExit();
 }
