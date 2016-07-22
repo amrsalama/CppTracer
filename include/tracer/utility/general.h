4e95464bbcabd19be5866ba12e4425492b27a073
Amr Salama <amr.mo.salama@gmail.com>
2016-07-22 16:08:24 +0200

    Separate declaration and definition of utilities

--- include/tracer/utility/general.h
+++ include/tracer/utility/general.h
@@ -2,62 +2,25 @@
 #define INCLUDE_TRACER_UTILITY_GENERAL_H_
 
 #include <string>
-#include <sstream>
-#include <iomanip>
-#include <limits>
 
 namespace tracer {
 namespace utility {
 namespace general {
 
-  // Converts data types to string to visualize them.
-  // Great example from "isocpp.org",
-  // LINK: https://isocpp.org/wiki/faq/templates#template-specialization-example
-  template<typename T>
-  inline std::string stringify(const T& data) {
-      std::ostringstream string_stream;
-      string_stream << data;
-      return string_stream.str();
-  }
+// Converts data types to string to visualize them.
+// Great example from "isocpp.org",
+// LINK: https://isocpp.org/wiki/faq/templates#template-specialization-example
+template<typename T> inline std::string stringify(const T& data);
 
-  // Template specialization for bool type,
-  // convert bool to "true" and "false" over "1" and "0".
-  template<>
-  inline std::string stringify<bool>(const bool& data) {
-    std::ostringstream string_stream;
-    string_stream << std::boolalpha << data;
-    return string_stream.str();
-  }
+// Template specialization for bool type,
+// convert bool to "true" and "false" over "1" and "0".
+template<> inline std::string stringify<bool>(const bool& data);
 
-  // Template specialization for double type,
-  // the floating point output contains all possible digits.
-  template<>
-  inline std::string stringify<double>(const double& data) {
-    const int sigdigits = std::numeric_limits<double>::digits10;
-    std::ostringstream string_stream;
-    string_stream << std::setprecision(sigdigits) << data;
-    return string_stream.str();
-  }
-
-  // Template specialization for float type,
-  // the floating point output contains all possible digits.
-  template<>
-  inline std::string stringify<float>(const float& data) {
-    const int sigdigits = std::numeric_limits<float>::digits10;
-    std::ostringstream string_stream;
-    string_stream << std::setprecision(sigdigits) << data;
-    return string_stream.str();
-  }
-
-  // Template specialization for long double type,
-  // the floating point output contains all possible digits.
-  template<>
-  inline std::string stringify<long double>(const long double& data) {
-    const int sigdigits = std::numeric_limits<long double>::digits10;
-    std::ostringstream string_stream;
-    string_stream << std::setprecision(sigdigits) << data;
-    return string_stream.str();
-  }
+// Template specialization for floating point types,
+// the floating point output contains all possible digits.
+template<> inline std::string stringify<double>(const double& data);
+template<> inline std::string stringify<float>(const float& data);
+template<> inline std::string stringify<long double>(const long double& data);
 
 }  // namespace general
 }  // namespace utility
