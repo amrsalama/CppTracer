7a8dc0f692d64962a945fcc332e7f5649b24f34b
Amr Salama <amr.mo.salama@gmail.com>
2016-07-24 23:53:00 +0200

    Support '\n', '\t' whitespace character

--- include/tracer/impl/core/utility/gl.h
+++ include/tracer/impl/core/utility/gl.h
@@ -8,6 +8,8 @@
 //
 // Implementation of gl.h utility header file.
 
+#include <algorithm>
+#include <iostream>
 #include <string>
 #include "GL/gl.h"
 #include "GL/freeglut.h"
@@ -25,9 +27,9 @@ void draw_rectangle(float x,
                     float width,
                     float height,
                     const T& data,
-                    const Color& background_color = Color(70, 70, 70),
-                    const Color& font_color = Color(255, 255, 255),
-                    int padding = 2) {
+                    const Color& background_color,
+                    const Color& font_color,
+                    int padding) {
     if (x < 0) x = 0;  // can't be less than 0
     if (y < 0) y = 0;  // can't be less than 0
 
@@ -51,9 +53,55 @@ void draw_rectangle(float x,
               font_color.get_blue(),
               font_color.get_alpha());
     std::string text = utility::general::stringify(data);
-    glRasterPos2f(x + (width  / 2.0) - ((9 * text.size()) / 2.0),  // Center
-                  y + (height / 2.0) - (15 / 2.0));                // Middle
+
+    // Support tab '\t' character in strings.
+    // Convert '\t' to 2 spaces.
+    int index = 0;
+    while (true) {
+      index = text.find("\t", index);
+      if (index == std::string::npos) break;
+      text.replace(index, 1, "  ");
+      index += 2;
+    }
+
+    // Support new line '\n' character in strings.
+    int endl_count = std::count(text.begin(), text.end(), '\n');
+
+    // Calculate maximum line width within the text.
+    // Width here means number of characters.
+    int max_line_width;
+    if (endl_count > 0) {
+      max_line_width = 0;
+      int temp_line_width = 0;
+      for (int i = 0; i < text.size(); i++) {
+        if (text[i] == '\n') {
+          max_line_width = std::max(max_line_width, temp_line_width);
+          temp_line_width = 0;
+          continue;
+        }
+        temp_line_width++;
+      }
+      max_line_width = std::max(max_line_width, temp_line_width);
+    } else {
+      max_line_width = text.size();
+    }
+
+    // Calculate the position of the text.
+    int text_x_pos = x + ((width  - (9 * max_line_width)) / 2.0);    // Center
+    int text_y_pos = y + ((height - 15 + (15 * endl_count)) / 2.0);  // Middle
+    if (text_x_pos < 0) text_x_pos = 0;  // can't be less than 0
+    if (text_y_pos < 0) text_y_pos = 0;  // can't be less than 0
+
+    glRasterPos2f(text_x_pos, text_y_pos);
     for (int i = 0; i < text.size(); i++) {
+        // Support '\n',
+        // if the next char is '\n' move to the next line.
+        if (text[i] == '\n') {
+          text_y_pos -= 15;
+          glRasterPos2f(text_x_pos, text_y_pos);
+          continue;
+        }
+
         glutBitmapCharacter(GLUT_BITMAP_9_BY_15, text[i]);
     }
 }
